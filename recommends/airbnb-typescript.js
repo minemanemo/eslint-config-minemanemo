fb900b026b89e8f52b5ad632290b4d4c6584675d
이민회(Minhoe Lee)/Payment데이터팀/11ST <minhoe.lee@sk.com>
2022-01-03 13:22:44 +0900

    Update Rule

--- recommends/airbnb-typescript.js
+++ recommends/airbnb-typescript.js
@@ -1,7 +1,23 @@
+// module.exports = {
+//   extends: ['airbnb-typescript', 'airbnb/hooks'],
+//   rules: {
+//     'import/no-extraneous-dependencies': 'off',
+//     '@typescript-eslint/no-floating-promises': 'off',
+//     '@typescript-eslint/explicit-module-boundary-types': 'off',
+//     '@typescript-eslint/no-use-before-define': 'off',
+//     'react/display-name': 'off',
+//   },
+// }
+
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
+    // 'airbnb-base',
+    // 'airbnb-typescript/base',
     'airbnb',
     'airbnb-typescript',
+    'airbnb/hooks',
     'plugin:import/recommended',
     'plugin:import/typescript',
   ],
--- typescript.js
+++ typescript.js
@@ -1,9 +1,9 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['./recommends/airbnb-typescript', './recommends/prettier', './recommends/next'].map(
-    (e) => require.resolve(e)
-  ),
+  extends: [
+    './recommends/airbnb-typescript',
+    // './recommends/prettier',
+    // './recommends/next'
+  ].map((e) => require.resolve(e)),
   env: {
     node: true,
     browser: true,
